faec6693c5a6afcde5831dbbda7297443891075d
luis.castillo <noba@outlook.com>
2021-03-04 10:20:50 -0600

    Created Maven Project

---
+++ src/test/java/com/luiscastillo/springjokesappv2/SpringJokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiscastillo.springjokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
